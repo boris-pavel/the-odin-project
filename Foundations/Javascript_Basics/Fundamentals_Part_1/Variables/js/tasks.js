a0e1a363a2b9dc2d1a29e0b806158ac08c344063
Boris Pavel <boris.pavel@gmx.com>
2023-03-26 14:46:48 +0300

    Solve variable tasks

---
+++ Foundations/Javascript_Basics/Fundamentals_Part_1/Variables/js/tasks.js
@@ -0,0 +1,17 @@
+/* Working with variables */
+
+let admin;
+let name;
+name = "John";
+admin = name;
+alert(admin);
+
+/* Giving the right name */
+
+const ourPlanet = "Earth";
+let currentVisitor;
+
+/* Uppercase const? */
+
+const BIRTHDAY = '18.04.1982';
+const age = someCode(BIRTHDAY);
