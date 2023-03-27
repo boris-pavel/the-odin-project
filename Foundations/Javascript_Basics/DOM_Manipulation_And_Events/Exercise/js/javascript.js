e2d1ae12f1d415c236cfde3f26dc80794e5b48db
Boris Pavel <boris.pavel@gmx.com>
2023-03-27 21:24:45 +0300

    Solve dom manipulation and events exercise

---
+++ Foundations/Javascript_Basics/DOM_Manipulation_And_Events/Exercise/js/javascript.js
@@ -0,0 +1,28 @@
+const para = document.createElement('p');
+
+para.style.color = 'red';
+para.textContent = 'Hey I\'m red!';
+
+const headerThree = document.createElement('h3');
+
+headerThree.style.color = 'blue';
+headerThree.textContent = 'I\'m a blue h3!';
+
+const div = document.createElement('div');
+
+div.style.cssText = 'border: 1px solid black; background-color: pink;';
+
+const headerOne = document.createElement('h1');
+
+headerOne.textContent = 'I\'m in a div';
+div.appendChild(headerOne);
+
+const paraTwo = document.createElement('p');
+
+paraTwo.textContent = 'ME TOO!';
+div.appendChild(paraTwo);
+
+const container = document.querySelector('.content');
+container.appendChild(para);
+container.appendChild(headerThree);
+container.appendChild(div);
