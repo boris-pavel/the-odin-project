a92bb84150d7dae3fee41aa4dcc8c1f932c7a0cb
Boris Pavel <boris.pavel@gmx.com>
2023-03-27 12:31:25 +0300

    Solve assignment

---
+++ Foundations/Javascript_Basics/Fundamentals_Part_3/Assignment/js/javascript.js
@@ -0,0 +1,15 @@
+function add7(num){
+    return num + 7;
+}
+
+function multiply(num1,num2){
+    return num1 * num2;
+}
+
+function capitalize(str){
+   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function lastLetter(str){
+    return str.charAt(str.length-1);
+}
